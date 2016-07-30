d273d876cb0ceab83043828ff479e63a850819a2
Estível <Estvel@NoteEstivel>
2016-07-30 16:37:07 -0300

    textos sem acentos

--- src/Principal.java
+++ src/Principal.java
@@ -16,14 +16,15 @@ public static void main(String[] args) {
 		Scanner QualLista;
 		String TipoLista = "";
 		
-		//Seleciona a lista de palavras para o jogo para o jogo.		
+		// Escolhe a lista de palavras		
 		while (!TipoLista.equals("i") && !TipoLista.equals("p")){
-			System.out.println("Você deseja importar um arquivo do tipo \".CSV\" com palavras para o jogo ou deseja usar a lista padrão de animais?"
-			+ "\nDigite \"P\" para a lista padrão ou \"I\" para importar uma lista e então confirme com \"ENTER\"");
+			System.out.println("Voce deseja importar um arquivo do tipo \".CSV\" com palavras para o jogo ou deseja usar a lista padrao de animais?"
+			+ "\nDigite \"P\" para a lista padrao ou \"I\" para importar uma lista e entao confirme com \"ENTER\"");
 			QualLista = new Scanner(System.in);
 			TipoLista = QualLista.nextLine().toLowerCase();
 		}
 		
+		//
 		if (TipoLista.equals("i")) {
 			boolean ok;
 			do {
@@ -36,7 +37,7 @@ public static void main(String[] args) {
 					
 				} catch (FileNotFoundException e) {
 					ok = false;
-					System.err.println("Arquivo ou caminho inválido.");
+					System.err.println("Arquivo ou caminho invalido.");
 					//e.printStackTrace();
 					
 				} 
@@ -44,21 +45,21 @@ public static void main(String[] args) {
 		} 
 		
 		FabricaMecanicaDoJogo FabricaJogo = new FabricaMecanicaDoJogo();
-		System.out.println("Em qual nível deseja jogar?");
-		System.out.println("Digite \"F\" para fácil ou \"D\" para difícil.");
+		System.out.println("Em qual nivel deseja jogar?");
+		System.out.println("Digite \"F\" para facil ou \"D\" para dificil.");
 		Scanner inDificuldade = new Scanner(System.in);
 		TipoDeJogo = inDificuldade.nextLine().toLowerCase();
 		
 		while (!TipoDeJogo.equals("f") && !TipoDeJogo.equals("d")){
-			System.err.println("Você precisa escolher uma dificuldade.\n");
-			System.out.println("Digite \"F\" para fácil ou \"D\" para difícil.");
+			System.err.println("Voce precisa escolher uma dificuldade.\n");
+			System.out.println("Digite \"F\" para facil ou \"D\" para dificil.");
 			inDificuldade = new Scanner(System.in);
 			TipoDeJogo = inDificuldade.nextLine().toLowerCase();
 		}	
 
 		if (TipoDeJogo.equals("f")) {
 			System.out.println("-----------------------------------");
-			System.out.println("Você escolheu o nível FÁCIL. Boa Sorte!");
+			System.out.println("Voce escolheu o nivel FACIL. Boa Sorte!");
 			System.out.println("-----------------------------------");
 			FabricaMecanicaDoJogo fabricaMecJogo = new FabricaMecanicaDoJogo();
 			jogo = fabricaMecJogo.EscolherMecanica(TipoDeJogo);
@@ -68,7 +69,7 @@ public static void main(String[] args) {
 			
 		}else if(TipoDeJogo.equals("d")){
 			System.out.println("-----------------------------------");
-			System.out.println("Você escolheu o nível DIFÍCIL. Boa Sorte!");
+			System.out.println("Voce escolheu o nivel DIFICIL. Boa Sorte!");
 			System.out.println("---------------------");
 			FabricaMecanicaDoJogo fabricaMecJogo = new FabricaMecanicaDoJogo();
 			jogo = fabricaMecJogo.EscolherMecanica(TipoDeJogo);
@@ -86,12 +87,12 @@ public static void main(String[] args) {
 
 			if (jogo.PalavraCorreta(palavra, tentativa)) {
 				System.out.println("-----------------------------------");
-				System.out.println("Você ACERTOU e ganhou " + jogo.Pontos() + " pontos!");
+				System.out.println("Voce ACERTOU e ganhou " + jogo.Pontos() + " pontos!");
 				System.out.println("Total de pontos conquistados: " + jogo.TotalDePontos());
 			} else {
 				if (jogo.ContagemDeVidas() >= 0) {
 					System.out.println("-----------------------------------");
-					System.out.println("Você errou a palavra, mas não desanime, você ainda possui "
+					System.out.println("Voce errou a palavra, mas nao desanime, voce ainda possui "
 							+ jogo.ContagemDeVidas() + " chances!");
 					System.out.println("Total de pontos conquistados: " + jogo.TotalDePontos());
 				}
@@ -99,11 +100,11 @@ public static void main(String[] args) {
 		} while ((!jogo.FimDeJogo()));
 
 		if (jogo.ContagemDeVidas() < jogo.GetVidasInciciais()) {
-			System.err.println("FIM DE JOGO! VOCÊ PERDEU! =( \n");
+			System.err.println("FIM DE JOGO! VOCE PERDEU! =( \n");
 			System.out.println("Total de Pontos: " + jogo.TotalDePontos());
 		}
 		if (jogo.TotalDePontos() >= jogo.GetPontosParaVitoria()) {
-			System.err.println("\nVOCÊ VENCEU O JOGO! \\o/ Uhuuu");
+			System.err.println("\nVOCE VENCEU O JOGO! \\o/ Uhuuu");
 			System.out.println("Total de Pontos: " + jogo.TotalDePontos());
 		}
 }
